9efade6a92a4fbcdda6a58151ac11747207aa7aa
Alex Belik <aleksandras41@gmail.com>
2017-01-05 00:08:07 +0200

    add function to display courses, add div, h3, and img (along with proper source and ahref) accoring to the provided json

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,4 +1,5 @@
 $(function() {
+
   $.ajax({
     url: 'https://www.codeschool.com/users/Aleksandras.json',
     dataType: 'jsonp',
@@ -6,6 +7,30 @@ $(function() {
       // handle response
     }
   });
-  // your code will go here
 
+  //Function to fetch json array and display course details with its help
+  function displayCourses(courses) {
+  	var badges = $('#badges');
+  	courses.forEach(function(course) {
+
+  		var div = $('<div />', {
+  			'class': 'course'
+  		}).appendTo($badges);
+
+  		$('<h3 />', {
+  			src: course.title
+  		}).appendTo($div);
+  		
+  		$('<img />', {
+  			src: course.badge
+  		}).appendTo($div);
+
+  		$('<a />', {
+  			'class': 'btn btn-primary',
+  			target: '_blank',
+  			href: course.url,
+  			text: 'Go to Course'
+  		})appendTo($div);
+  	});
+  }
 });
